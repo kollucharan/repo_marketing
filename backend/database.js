eb047bb0c6dea2cb18db2062d235eb108220c71f
kollucharan <132676020+kollucharan@users.noreply.github.com>
2025-03-27 15:05:33 +0530

    added database

---
+++ backend/database.js
@@ -0,0 +1,11 @@
+// database.js
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export default pool;
--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,31 @@ import cors from "cors";
 import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
+import pool from './database.js';
 
 const PORT = 5000;
  const app =express();
 app.use(express.json());
-app.use(cors());
+app.use(cors()); 
+
+const insertQuery = `INSERT INTO JobRoles(Job_description) VALUES ($1)`
 
 app.post("/generate", async (req, res) => {
   const { role } = req.body;
   if (!role) {
     return res.status(400).json({ error: "Job role is required" });
   }
+    try {
+      
+      const client = await pool.connect();
+      await client.query(insertQuery, [role]);
+      client.release();
+    }
+    catch (error) {
+      
+      res.status(500).json({ error: "server issue try again later" });
+
+     }
   
   try {
     const response = await axios.post(
